a1acd4785df045e079039c317a95068340782aaf
alnahian2003 <a.alnahian2003@gmail.com>
2022-10-01 01:13:33 +0600

    Sending emails and notifications completed

--- app/Models/Chirp.php
+++ app/Models/Chirp.php
@@ -2,6 +2,8 @@
 
 namespace App\Models;
 
+use App\Events\ChirpCreated;
+
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -11,6 +13,10 @@ class Chirp extends Model
 
     protected $fillable = [
         'message'
+    ];
+
+    protected $dispatchesEvents = [
+        'created' => ChirpCreated::class,
     ];
 
     public function user()
